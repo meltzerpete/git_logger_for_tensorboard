47b1ad0ed1d8dcbd8a617b9bf955a53eda366120
Pete Meltzer <pete.meltzer@autodesk.com>
2022-12-21 16:15:44 +0000

    update demo

--- git_logger_for_tensorboard/demo.py
+++ git_logger_for_tensorboard/demo.py
@@ -14,19 +14,20 @@
 # ==============================================================================
 """Demo code."""
 from pytorch_lightning.loggers import TensorBoardLogger
+from torch.utils.tensorboard import SummaryWriter
 
-from git_logger_for_tensorboard.git_logger import GitLightningLogger
+from git_logger_for_tensorboard.git_logger import GitLightningLogger, GitLogger
 
 
 def main():
+    # with pytorch
     summary_writer = SummaryWriter(log_dir='demo_logs')
-    GitLogger(summary_writer).log('train')
+    GitLogger(summary_writer).log()
 
+    # with pytorch_lightning
     tb_logger = TensorBoardLogger(save_dir='lightning_logs',
                                   name='my_model')
-    GitLightningLogger(tb_logger).log('test')
-
-    tb_logger.experiment.add_scalar('my_scalar', 7)
+    GitLightningLogger(tb_logger).log()
 
 
 if __name__ == "__main__":
